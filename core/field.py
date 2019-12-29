b0db29d6edc3ec3cfdc1b9a3e581086ef22a3ff0
makeitokay <andrey.mu11@yandex.ru>
2019-12-29 20:53:56 +0500

    Изменил ширину экрана

--- core/field.py
+++ core/field.py
@@ -4,24 +4,24 @@ from .settings import SCREEN_WIDTH, SCREEN_HEIGHT
 
 class Field:
     CELL_SIZE = 50
+    WIDTH = (SCREEN_WIDTH - 300) // CELL_SIZE
+    HEIGHT = SCREEN_HEIGHT // CELL_SIZE
 
     def __init__(self, surface):
-        self.width = SCREEN_WIDTH // self.CELL_SIZE
-        self.height = SCREEN_HEIGHT // self.CELL_SIZE
         self.surface = surface
-        self.cells = [[0] * self.width for _ in range(self.height)]
-        self.colors = [[BLACK] * self.width for _ in range(self.height)]
+        self.cells = [[0] * self.WIDTH for _ in range(self.HEIGHT)]
+        self.colors = [[BLACK] * self.WIDTH for _ in range(self.HEIGHT)]
 
     def render(self):
-        for i in range(self.height):
-            for j in range(self.width):
+        for i in range(self.HEIGHT):
+            for j in range(self.WIDTH):
                 cell = pg.Rect(j * self.CELL_SIZE, i * self.CELL_SIZE, self.CELL_SIZE, self.CELL_SIZE)
                 self.cells[i][j] = cell
-                pg.draw.rect(self.surface, BLACK, cell, 1)
+                pg.draw.rect(self.surface, WHITE, cell, 1)
 
     def get_cell(self, mouse_pos):
-        for i in range(self.height):
-            for j in range(self.width):
+        for i in range(self.HEIGHT):
+            for j in range(self.WIDTH):
                 if self.cells[i][j].contains(pg.Rect(*mouse_pos, 1, 1)):
                     return i, j
         return None
