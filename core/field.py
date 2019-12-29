479d866ec315855cf6a44fddfa93061e3d9f18a7
makeitokay <andrey.mu11@yandex.ru>
2019-12-29 18:05:27 +0500

    Обработка кликов на ячейки

--- core/field.py
+++ core/field.py
@@ -6,21 +6,33 @@ class Field:
     CELL_SIZE = 50
 
     def __init__(self, surface):
-        self.width = SCREEN_WIDTH
-        self.height = SCREEN_HEIGHT
+        self.width = SCREEN_WIDTH // self.CELL_SIZE
+        self.height = SCREEN_HEIGHT // self.CELL_SIZE
         self.surface = surface
         self.cells = [[0] * self.width for _ in range(self.height)]
+        self.colors = [[BLACK] * self.width for _ in range(self.height)]
 
     def render(self):
         for i in range(self.height):
             for j in range(self.width):
                 cell = pg.Rect(j * self.CELL_SIZE, i * self.CELL_SIZE, self.CELL_SIZE, self.CELL_SIZE)
                 self.cells[i][j] = cell
-                pg.draw.rect(self.surface, WHITE, cell, 1)
+                pg.draw.rect(self.surface, BLACK, cell, 1)
 
     def get_cell(self, mouse_pos):
-        for i in range(len(self.cells)):
-            for j in range(len(self.cells[i])):
+        for i in range(self.height):
+            for j in range(self.width):
                 if self.cells[i][j].contains(pg.Rect(*mouse_pos, 1, 1)):
                     return i, j
         return None
+
+    def on_click(self, cell):
+        x, y = cell
+        color = BLACK if self.colors[x][y] == WHITE else WHITE
+        self.colors[x][y] = color
+        pg.draw.rect(self.surface, color, self.cells[x][y])
+
+    def get_click(self, mouse_pos):
+        cell = self.get_cell(mouse_pos)
+        if cell:
+            self.on_click(cell)
--- core/game.py
+++ core/game.py
@@ -1,5 +1,6 @@
 import pygame as pg
 
+from core.utils import terminate
 from .settings import FPS
 from .field import Field
 
@@ -8,12 +9,20 @@ class Game:
         self.surface = surface
         self.clock = pg.time.Clock()
         self.field = Field(self.surface)
-        self.field.render()
 
     def run(self):
         while True:
+            self.events()
             self.update()
             self.clock.tick(FPS)
 
     def update(self):
+        self.field.render()
         pg.display.update()
+
+    def events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                terminate()
+            elif event.type == pg.MOUSEBUTTONUP:
+                self.field.get_click(event.pos)
